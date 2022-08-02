8ff52e65aa1105639373f928d24421e929cdde3a
Ashwarya142 <110378739+Ashwarya142@users.noreply.github.com>
2022-08-02 20:53:55 +0530

    By Git

--- NETB-Git-02/src/DEMO_1.java
+++ NETB-Git-02/src/DEMO_1.java
@@ -1,9 +1,11 @@
 
 public class DEMO_1 {
-	public static void main(String[] args) {
-		
-		System.out.println("Hello");
-		
+	public static void main(String[] args) 
+       {
+	System.out.println("Hello");
 	}
-
+        public void m1()
+	{
+		System.out.println("Hello im in git");
+	}
 }
